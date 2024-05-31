be9bf84ca0c7327683708e996a6c4e5d2d3d55e9
lukac <luka.cheshlarov@students.finki.ukim.mk>
2024-05-31 16:35:35 +0200

    Add project files.

---
+++ Models/Doctor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lab3.Models
+{
+    public class Doctor
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public Hospital WorksAt { get; set; }
+        public virtual ICollection<Patient> Patients { get; set; }
+
+    }
+}
---
+++ Models/Patient.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace Lab3.Models
+{
+    public class Patient
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+        [Required]
+        [StringLength(5)]
+        public string PatientCode { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string Description { get; set; }
+
+        //public static IEnumerable<Object> Genders = new List<Object> {
+        //     new Object {
+        //        GenderId = 1,
+        //        Name = "Male"
+        //     },
+        //    new Color {
+        //        GenderId = 2,
+        //        Name = "Female"
+        //    }
+        //};
+    }
+}
